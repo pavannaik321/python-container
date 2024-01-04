8f46ce84c6f79282b58da02a82cfb6de17b3bb03
Pavan Naik <82699802+pavannaik321@users.noreply.github.com>
2024-01-04 22:38:21 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,9 @@
+# An object of Flask class is our WSGI application.
+from flask import Flask
+helloworld = Flask(__name__)
+@helloworld.route("/")
+def run():
+    return "{\"message\":\"Hey there python\"}"
+
+if __name__ == "__main__":
+    helloworld.run(host="0.0.0.0",port=int("3000"),debug=True)
